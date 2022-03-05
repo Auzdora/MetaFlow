cd7a108611a11317814e7d52dbde1647c8c30fad
Melrose-Lbt <1076388287@qq.com>
2022-03-05 17:02:29 +0800

    Create _Optimizers.py

---
+++ opt/_Optimizers.py
@@ -0,0 +1,41 @@
+"""
+    Copyright © 2022 Melrose-Lbt
+    All rights reserved
+
+    Filename: _Optimizers.py
+    Description: This file provides different types of optimizers.
+
+    Created by Melrose-Lbt 2022-2-28
+"""
+
+
+class Optimizer:
+    pass
+
+
+class BGD(Optimizer):
+    pass
+
+
+class SGD(Optimizer):
+    pass
+
+
+class MiniBGD(Optimizer):
+    pass
+
+
+class Momentum(Optimizer):
+    pass
+
+
+class AdaGrad(Optimizer):
+    pass
+
+
+class RMSProp(Optimizer):
+    pass
+
+
+class Adam(Optimizer):
+    pass
