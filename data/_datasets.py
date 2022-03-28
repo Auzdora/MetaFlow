93b1be698280ee48722f0a987e391a8c4d228fdd
Melrose-Lbt <1076388287@qq.com>
2022-03-28 23:19:03 +0800

    Update _datasets.py

--- data/_datasets.py
+++ data/_datasets.py
@@ -3,15 +3,46 @@
     All rights reserved
 
     Filename: _datasets.py
-    Description: This file provides a demo for MetaFlow.
+    Description: _datasets.py provides an abstract class for every kind of dataset.
+        You could create your own dataset by inheriting from Dataset class. But you
+        have to override __getitem__ and __len__ method to make sure that everything
+        is all set.
+            When you need to create a dataset defined by you self, and you wanna use
+        MetaFlow as a framework and use MetaFlow.data.DataLoader to load your data,
+        you have to use this class to pack your dataset. Then you could deliver it to
+        DataLoader.
 
     Created by Melrose-Lbt 2022-3-28
 """
 
 
 class Dataset:
+    """
+        An abstract class, base class for all Dataset subclass.
+
+
+        For examples:
+            class Mydata(Dataset):
+                def __init__(self, dataset):
+                    self.dataset = dataset
+
+                def __getitem__(self, index):
+                    return self.dataset[index]
+
+                def __len__(self):
+                    return len(self.dataset)
+    """
     def __getitem__(self, index):
+        """
+            When a subclass inherits from Dataset, you have to override this method.
+            This method is an iterator.
+            You have to make sure it returns inputs(data) and targets(label).
+
+        """
         raise NotImplementedError
 
     def __len__(self):
+        """
+            When a subclass inherits from Dataset, you have to override this method.
+        """
         raise NotImplementedError
