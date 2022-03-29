c6b2abdf3a93dae000919512bafbbbe09de3285e
Melrose-Lbt <69419952+Melrose-Lbt@users.noreply.github.com>
2022-03-29 14:47:06 +0800

    update demo3

---
+++ test/demo3.py
@@ -0,0 +1,11 @@
+"""
+    Copyright © 2022 Melrose-Lbt
+    All rights reserved
+
+    Filename: demo3.py
+    Description: This demo tests how to use 'data' module to pack your data
+        and feed them in to your neural networks.
+
+    Created by Melrose-Lbt 2022-3-29
+"""
+
