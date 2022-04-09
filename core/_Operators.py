Softmax and LossMSE fix

core/_Operators.py
@@ -210,7 +210,14 @@ class SoftMax(Operator):
         return _out
 
     def compute_jacobi(self, parent):
-        pass
+        _tCounter, _tSummer = 0, 0
+        for _subTensor in self.value:
+            diag = _subTensor.T
+            Y = np.diag(diag.squeeze(0))
+            Y_2 = np.matmul(_subTensor, _subTensor.T)
+            _tSummer += Y - Y_2
+            _tCounter += 1
+        return _tSummer / _tCounter
 
 
 if __name__ == "__main__":

layers/linear_layers.py
@@ -16,7 +16,7 @@ class Linear(Modules):
         Linear layers (Fully connected layers) for neural network.
     """
 
-    def __init__(self, in_features, out_features, bias=True, init_para='xavier_normal'):
+    def __init__(self, in_features, out_features, bias=True, init_para='xavier_uniform'):
         """
         :param in_features:
         :param out_features:
@@ -61,6 +61,7 @@ class Linear(Modules):
             Reset network's parameters. To make network easier to learn.
         :return:
         """
+        # TODO: Confirm and pack them into a single file
         if self.init_para == 'normal':
             self.weight.value = np.random.normal(loc=0., scale=1., size=(1, self.out_features, self.in_features))
             if self.bias is None:

loss_fn/_loss_fns.py
@@ -7,6 +7,8 @@
 
     Created by Melrose-Lbt 2022-3-5
 """
+from numpy import ndarray
+
 from core import Tensor
 import numpy as np
 
@@ -27,24 +29,36 @@ class LossMSE(BaseLoss):
         Mean square error loss function.
     """
     def __init__(self, label, outputs):
-        self.label = np.expand_dims(label, axis=1)
+
+        if isinstance(label, ndarray):
+            pass
+        else:
+            label = np.array(label)
+
+        if len(label.shape) == 1:
+            self.label = np.expand_dims(label, axis=0)
+        else:
+            self.label = label.T
 
         # Number of samples
-        self.N = len(label)
+        self.batch = len(label)
 
-        self.jacobi_coef = 2
+        self.jacobi_coef = 1
         super(LossMSE, self).__init__(*[outputs], grad_fn='<LossMSE>', special_op=True)
 
     def compute_value(self, *args):
         # TODO: Add assert to make sure label dim equals to output dim
         outputs = self.connect_tensor(args[0])
-        return (((outputs[0].squeeze(-1) - self.label)**2).sum())/self.N
+        _tSummer = 0
+        for index, output in enumerate(outputs[0]):
+            _tSummer += ((output - np.expand_dims(self.label[:, index], axis=1)) ** 2).mean()
+        return _tSummer/self.batch
 
     def compute_jacobi(self, parent):
         # TODO:Explain here why we need add .T
         _tCounter, _tSummer = 0, 0
         for label_index, _subTensor in enumerate(parent.value):
-            _tSummer += self.jacobi_coef * (_subTensor - self.label[label_index]).T
+            _tSummer += self.jacobi_coef * (_subTensor - np.expand_dims(self.label[:, label_index], axis=1)).T
             _tCounter += 1
         return _tSummer/_tCounter
 